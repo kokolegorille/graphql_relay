eb045192e70e4ee5f2f309461269dff09bae3e87
koko.le.gorille <koko.le.gorille@gmail.com>
2017-08-29 05:02:59 +0200

    Clean up app.js

--- src/app.js
+++ src/app.js
@@ -5,42 +5,6 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
-// import {
-//   QueryRenderer,
-//   graphql,
-// } from 'react-relay';
-// 
-// import environment from './environment';
-// import Store from './components/store';
-//
-// main();
-// 
-// function main() {
-//   const app = document.getElementById('app');
-//   render(
-//     <QueryRenderer
-//       environment={environment}
-//       query={graphql.experimental`
-//         query appQuery($limit: Int) {
-//           store {
-//             ...store_store @arguments(limit: $limit)
-//           }
-//         }
-//       `}
-//       variables={{limit: 2}}
-//       render={({error, props}) => {
-//         if (error) {
-//           return <div>{error.message}</div>;
-//         } else if (props) {
-//           return <Store store={props.store} />;
-//         }
-//         return <div>Loading...</div>;
-//       }}
-//     />,
-//     app
-//   );
-// }
-
 main();
 
 function main() {
