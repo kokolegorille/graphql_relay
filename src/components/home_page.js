83ccfd7f3a3ec073c67cbb22fa77b8df8888909a
koko.le.gorille <koko.le.gorille@gmail.com>
2017-09-01 06:50:13 +0200

    Add base pagination

--- src/components/home_page.js
+++ src/components/home_page.js
@@ -32,36 +32,3 @@ const HomePage = () => (
   
 export default HomePage;
 
-// import React from 'react';
-//
-// import {
-//   QueryRenderer,
-//   graphql,
-// } from 'react-relay';
-//
-// import environment from '../environment';
-// import Store from './store';
-//
-// const HomePage = () => (
-//   <QueryRenderer
-//     environment={environment}
-//     query={graphql.experimental`
-//       query homePageQuery($limit: Int) {
-//         store {
-//           ...store_store @arguments(limit: $limit)
-//         }
-//       }
-//     `}
-//     variables={{limit: 5}}
-//     render={({error, props}) => {
-//       if (error) {
-//         return <div>{error.message}</div>;
-//       } else if (props) {
-//         return <Store store={props.store} />;
-//       }
-//       return <div>Loading...</div>;
-//     }}
-//   />
-// );
-//
-// export default HomePage;
--- src/components/store.js
+++ src/components/store.js
@@ -1,46 +1,151 @@
-import React from 'react';
-import { createFragmentContainer, graphql } from 'react-relay';
+import React, {Component} from 'react';
+import { createPaginationContainer, graphql } from 'react-relay';
 import PageInfo from './pageInfo';
 import Video from './video';
 
-const Store = ({store}) => (
-  <div>  
-    <ul>
-      {
-        store.videos.edges.map(edge => <Video video={edge.node} />)
-      }
-    </ul>
-    <hr/>
-    <p>Total count : {store.videos.totalCount}</p>
-    <PageInfo pageInfo={store.videos.pageInfo} />
-  </div>
-);
-
-export default createFragmentContainer(Store, {
-  store: graphql.experimental`
-    fragment store_store on Store 
-    @argumentDefinitions(
-      count: {type: "Int", defaultValue: 3}
-    )
-    {
-      videos(first: $count) {
-        edges {
-          node {
-            id,
-            ...video_video
+class Store extends Component {
+  render() {
+    const {store} = this.props;
+    return (
+      <div>  
+        <ul>
+          {
+            store.videos.edges.map(edge => <Video video={edge.node} />)
           }
-        },
-        totalCount,
-        pageInfo {
-          hasPreviousPage,
-          hasNextPage,
-          startCursor,
-          endCursor
+        </ul>
+        <button
+          onClick={this._loadMore}
+          title="Load More">
+          Load more...
+        </button>
+        <hr/>
+        <p>Total count : {store.videos.totalCount}</p>
+        <PageInfo pageInfo={store.videos.pageInfo} />
+      </div>
+    );
+  }
+  
+  _loadMore = () => {
+    if (!this.props.relay.hasMore() || this.props.relay.isLoading()) {
+      return;
+    }
+    
+    this.props.relay.loadMore(
+      3, // Fetch the next 3 feed items
+      e => {console.log(e)}
+    );
+  }
+}
+
+export default createPaginationContainer(
+  Store, 
+  {
+    store: graphql.experimental`
+      fragment store_store on Store 
+      @argumentDefinitions(
+        count: {type: "Int", defaultValue: 20}
+        cursor: {type: "String"}
+      ) 
+      {
+        videos(
+          first: $count
+          after: $cursor
+          ) @connection(key: "store_videos") {
+          edges {
+            node {
+              id,
+              ...video_video
+            }
+          },
+          totalCount,
+          pageInfo {
+            hasPreviousPage,
+            hasNextPage,
+            startCursor,
+            endCursor
+          }
         }
       }
-    }
-  `,
-});
+    `,
+  },
+  {
+    direction: 'forward',
+    getConnectionFromProps(props) {
+      return props.store && props.store.videos;
+    },
+    getFragmentVariables(prevVars, totalCount) {
+      return {
+        ...prevVars,
+        count: totalCount,
+      };
+    },
+    getVariables(props, {count, cursor}, fragmentVariables) {
+      return {
+        count,
+        cursor,
+        // in most cases, for variables other than connection filters like
+        // `first`, `after`, etc. you may want to use the previous values.
+        // orderBy: fragmentVariables.orderBy,
+      };
+    },
+    query: graphql.experimental`
+      query storeQuery(
+        $count: Int!
+        $cursor: String
+      ) {
+        store {
+          videos(
+            first: $count
+            after: $cursor
+          ) @connection(key: "store_videos") {
+            edges {
+              node {
+                id,
+                ...video_video
+              }
+            },
+            totalCount,
+            pageInfo {
+              hasPreviousPage,
+              hasNextPage,
+              startCursor,
+              endCursor
+            }
+          }
+        }
+      }
+    `
+  }
+);
+
+// export default createFragmentContainer(Store, {
+//   store: graphql.experimental`
+//     fragment store_store on Store
+//     @argumentDefinitions(
+//       count: {type: "Int", defaultValue: 3}
+//     )
+//     {
+//       videos(first: $count) {
+//         edges {
+//           node {
+//             id,
+//             ...video_video
+//           }
+//         },
+//         totalCount,
+//         pageInfo {
+//           hasPreviousPage,
+//           hasNextPage,
+//           startCursor,
+//           endCursor
+//         }
+//       }
+//     }
+//   `,
+// });
+
+
+
 
 // import React from 'react';
 // import { createFragmentContainer, graphql } from 'react-relay';
